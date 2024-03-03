6a2e1b439ecefa902b134aa5efa336d82fde5b9e
FifiLyu <fifilyu@gmail.com>
2024-03-03 16:44:08 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-docker buildx build -t fifilyu/jenkins:latest .
+tag=$(date +'%Y%m%d-%H%M%S')
+
+docker images | grep -E "^fifilyu/jenkins   ${tag}"
+
+if [ $? -eq 0 ]; then
+    echo "[错误] 指定的Docker镜像tag已经存在：${tag}"
+    exit 1
+fi
+
+echo "[信息] 构建Docker镜像：fifilyu/jenkins:${tag}"
+docker buildx build -t fifilyu/jenkins:${tag} -t fifilyu/jenkins:latest .
